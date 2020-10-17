5e971fb8be453adf7a31087ff192748f466a254c
Frostrix <frostrixz@gmail.com>
2020-10-17 16:24:04 +1300

    Removed usage command

--- Commands/UtilityCommands/_Initialization.cs
+++ Commands/UtilityCommands/_Initialization.cs
@@ -1,28 +1,17 @@
 ﻿using Dexter.Configuration;
 using Dexter.Core.Abstractions;
 using Dexter.Services;
-using Discord.Commands;
-using System.Diagnostics;
 
 namespace Dexter.Commands.UtilityCommands {
     public partial class UtilityCommands : ModuleD {
-
-        private readonly CommandService CommandService;
-
-        private readonly PerformanceCounter CPUCounter;
-
-        private readonly PerformanceCounter MemCounter;
 
         private readonly LoggingService LoggingService;
 
         private readonly BotConfiguration BotConfiguration;
 
-        public UtilityCommands(CommandService _CommandService, BotConfiguration _BotConfiguration, LoggingService _LoggingService) {
-            CommandService = _CommandService;
+        public UtilityCommands(BotConfiguration _BotConfiguration, LoggingService _LoggingService) {
             BotConfiguration = _BotConfiguration;
             LoggingService = _LoggingService;
-            CPUCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
-            MemCounter = new PerformanceCounter("Memory", "Available MBytes");
         }
 
     }

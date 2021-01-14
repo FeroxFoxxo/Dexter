1402a8a05a9641c646386b840eaf2dd1a7607b58
Frostrix <frostrixz@gmail.com>
2021-01-14 20:33:27 +1300

    Bugfix to issue command - now notifies if points == -4.

--- Dexter/Commands/ModeratorCommands/IssueInfractionCommand.cs
+++ Dexter/Commands/ModeratorCommands/IssueInfractionCommand.cs
@@ -134,7 +134,7 @@ namespace Dexter.Commands {
             if (DexterProfile.InfractionAmount - PointsDeducted <= -4 && DexterProfile.InfractionAmount > -4)
                 await BuildEmbed(EmojiEnum.Wut)
                     .WithTitle($"Frequent Rulebreaker Inbound!!!")
-                    .WithDescription($"Haiya! It seems as though the user {User.GetUserInformation()} is currently standing on {DexterProfile.InfractionAmount}. Perhaps this is something the {ModerationConfiguration.AdministratorMention}s can dwell on. <3")
+                    .WithDescription($"Haiya! It seems as though the user {User.GetUserInformation()} is currently standing on {DexterProfile.InfractionAmount - PointsDeducted}. Perhaps this is something the {ModerationConfiguration.AdministratorMention}s can dwell on. <3")
                     .WithCurrentTimestamp()
                     .SendEmbed(DiscordSocketClient.GetChannel(BotConfiguration.ModerationLogChannelID) as ITextChannel);
 

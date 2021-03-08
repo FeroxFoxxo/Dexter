3b96ceccb491a58bb43db526b9cabbb44eec5e3d
EnderDoom77 <enderdoom77@gmail.com>
2021-03-08 23:03:51 +0100

    Simplified usings

--- Dexter/Commands/FunCommands/CoinFlipCommand.cs
+++ Dexter/Commands/FunCommands/CoinFlipCommand.cs
@@ -2,9 +2,6 @@
 using Dexter.Extensions;
 using Discord.Commands;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Dexter.Commands {

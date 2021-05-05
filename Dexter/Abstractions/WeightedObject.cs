1923f9e46dc201e8797e9d6cc938bec45bf4bd41
EnderDoom77 <enderdoom77@gmail.com>
2021-05-05 14:31:11 +0200

    Initial setup and infrastructure

---
+++ Dexter/Abstractions/WeightedObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dexter.Abstractions {
+    
+    /// <summary>
+    /// A structure containing an object and a related value used for sorting.
+    /// </summary>
+    
+    public class WeightedObject {
+        /// <summary>
+        /// The object held in this instance.
+        /// </summary>
+        public object obj;
+        /// <summary>
+        /// The weight attached to this object.
+        /// </summary>
+        public double weight;
+
+        /// <summary>
+        /// Creates a new WeightedObject given a base object and its weight.
+        /// </summary>
+        /// <param name="obj">The object to hold.</param>
+        /// <param name="weight">The weight of the object.</param>
+
+        public WeightedObject(object obj = null, double weight = 0) {
+            this.obj = obj;
+            this.weight = weight;
+        }
+
+        /// <summary>
+        /// Sorts an enumerable collection of WeightedObjects by their weight.
+        /// </summary>
+        /// <param name="objs">The collection of objects.</param>
+        /// <param name="descending">Whether to sort in descending order.</param>
+        /// <returns>A list with the values in <paramref name="objs"/> sorted in ascending order if <paramref name="descending"/> is <see langword="false"/>, or otherwise in descending order.</returns>
+
+        public static List<WeightedObject> SortByWeight(IEnumerable<WeightedObject> objs, bool descending = false) {
+            List<WeightedObject> result = objs.ToList();
+
+            SortByWeightInPlace(result);
+            return result;
+        }
+
+        public static void SortByWeightInPlace(List<WeightedObject> objs, bool descending = false) {
+            objs.Sort((a, b) => (a.weight < b.weight ^ descending) ? -1 : 1);
+        }
+    }
+}

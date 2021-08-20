082fd4b362ffa94e8eff7d41bd531ff19be3a4ce
FeroxFoxxo <frostrixz@gmail.com>
2021-08-20 21:14:00 +1200

    Removed moderator ability to muzzle as the TypeReader will try parse text as being users, throwing errors on execution. As moderators don't use this command, there isn't need to create entirely new, custom type readers for this.

--- Dexter/Commands/MuzzleCommands/MuzzleCommand.cs
+++ Dexter/Commands/MuzzleCommands/MuzzleCommand.cs
@@ -27,15 +27,10 @@ namespace Dexter.Commands
         [Alias("muzzleme")]
         [CommandCooldown(60)]
 
-        public async Task MuzzleCommand([Optional] IGuildUser User)
+        public async Task MuzzleCommand()
         {
-            bool IsUserSpecified = User != null;
-
             IGuildUser MuzzledUser = Context.Guild.GetUser(Context.User.Id);
-
-            if (IsUserSpecified && (Context.User as IGuildUser).GetPermissionLevel(DiscordSocketClient, BotConfiguration) >= PermissionLevel.Moderator)
-                MuzzledUser = User;
-
+            
             await Muzzle(MuzzledUser);
 
             await Context.Channel.SendMessageAsync($"Muzzled **{MuzzledUser.Username}#{MuzzledUser.Discriminator}~!**");

df3523dacf9c2c70729139903a118228a5a0d287
FeroxFoxxo <frostrixz@gmail.com>
2021-09-30 19:24:57 +1300

    Update StringExtensions.cs

--- Dexter/Extensions/StringExtensions.cs
+++ Dexter/Extensions/StringExtensions.cs
@@ -81,7 +81,7 @@ namespace Dexter.Extensions
             if (Index != -1)
                 methodName = methodName.Substring(0, Index).Replace("<", "");
 
-            if (name.Contains("AsyncMethodBuilderCore") || name.Contains("AsyncTaskMethodBuilder") || name.Contains("EmbedExtensions") || name.Contains("AwaitTaskContinuation"))
+            if (name.Contains("AsyncMethodBuilderCore") || name.Contains("AsyncTaskMethodBuilder") || name.Contains("EmbedExtensions") || name.Contains("AwaitTaskContinuation") || name.Contains("ExecutionContext"))
                 return GetLastMethodCalled(SearchHeight + 1);
             else
                 return new KeyValuePair<string, string> ( name, methodName );

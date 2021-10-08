3fd2703efd8d90522fc09a414cb9ff7167ffe93f
Frostrix <frostrixz@gmail.com>
2021-10-08 15:39:52 +1300

    Fixed search command playback issue.

--- Dexter/Commands/MusicCommands/Search.cs
+++ Dexter/Commands/MusicCommands/Search.cs
@@ -5,6 +5,7 @@ using Discord;
 using Discord.Commands;
 using Fergun.Interactive;
 using Fergun.Interactive.Selection;
+using Humanizer;
 using Microsoft.Extensions.Logging;
 using SpotifyAPI.Web;
 using System;
@@ -298,6 +299,73 @@ namespace Dexter.Commands
 				}
 
 				await BuildEmbed(EmojiEnum.Unknown).GetQueuedTrack(track, player.Vueue.Count).SendEmbed(Context.Channel);
+			}
+		}
+
+		[Command("pause")]
+		[Alias("resume", "play", "unpause", "p")]
+		[Summary("Toggles whether this player is currently paused. Use while songs are playing to pause the player, use while a player is paused to resume it.")]
+		[MusicBotChannel]
+
+		public async Task PauseCommand()
+		{
+			if (!await LavaNode.SafeJoinAsync(Context.User, Context.Channel))
+			{
+				await BuildEmbed(EmojiEnum.Annoyed)
+					.WithTitle("Unable to pause the player!")
+					.WithDescription("Failed to join voice channel.\nAre you in a voice channel?").SendEmbed(Context.Channel);
+
+				return;
+			}
+
+			if (LavaNode.TryGetPlayer(Context.Guild, out var player))
+			{
+				if (player.PlayerState == PlayerState.Paused)
+				{
+					await player.ResumeAsync();
+					await BuildEmbed(EmojiEnum.Love)
+						.WithTitle("Resumed the player.")
+						.WithDescription($"Successfully resumed {player.Track.Title}")
+						.SendEmbed(Context.Channel);
+				}
+				else if (player.PlayerState == PlayerState.Playing)
+				{
+					await player.PauseAsync();
+					await BuildEmbed(EmojiEnum.Love)
+						.WithTitle("Paused the player.")
+						.WithDescription($"Successfully paused {player.Track.Title}")
+						.SendEmbed(Context.Channel);
+				}
+				else if (player.PlayerState == PlayerState.Stopped)
+				{
+					var track = player.Vueue.FirstOrDefault();
+
+					if (track is not null)
+					{
+						await player.PlayAsync(track);
+
+						await player.SkipAsync();
+					}
+
+					if (player.Track is not null && track is not null)
+						await BuildEmbed(EmojiEnum.Love)
+							.WithTitle("Resumed the player.")
+							.WithDescription($"Successfully resumed {player.Track.Title}")
+							.SendEmbed(Context.Channel);
+					else
+						await BuildEmbed(EmojiEnum.Love)
+							.WithTitle("Could not resume the player.")
+							.WithDescription($"No tracks currently in queue!")
+							.SendEmbed(Context.Channel);
+				}
+				else
+				{
+					await BuildEmbed(EmojiEnum.Annoyed)
+						.WithTitle("Unable to pause the player!")
+						.WithDescription("The player must be either in a playing or paused state to use this command.\n" +
+												   $"Current state is **{player.PlayerState.Humanize()}**.")
+						.SendEmbed(Context.Channel);
+				}
 			}
 		}
 	}
